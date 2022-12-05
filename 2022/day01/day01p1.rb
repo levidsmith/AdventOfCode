55df28065aa04289587ca4886f3379dd9bb59bbd
Levi D. Smith <me@levidsmith.com>
2022-12-04 22:46:51 -0500

    2022 day 1

---
+++ 2022/day01/day01p1.rb
@@ -0,0 +1,29 @@
+puts ARGV[0]
+
+bags = Array.new
+f = File.new(ARGV[0])
+
+iBag = 0
+f.each do | line |
+    puts "linei: #{line}"
+    iBag += line.to_i
+    
+    if (line.strip == "")
+        puts "Bag total: #{iBag}"
+        bags << iBag
+        puts "New bag"
+        iBag = 0
+    end
+end
+
+puts "Last bag"
+bags << iBag
+
+f.close()
+
+bags.sort!
+bags.each do | i |
+    puts i
+end
+
+puts "Max bag: #{bags.last}"
---
+++ 2022/day01/day01p2.rb
@@ -0,0 +1,40 @@
+puts ARGV[0]
+
+bags = Array.new
+f = File.new(ARGV[0])
+
+iBag = 0
+f.each do | line |
+    puts "linei: #{line}"
+    iBag += line.to_i
+    
+    if (line.strip == "")
+        puts "Bag total: #{iBag}"
+        bags << iBag
+        puts "New bag"
+        iBag = 0
+    end
+end
+
+puts "Last bag"
+bags << iBag
+
+f.close()
+
+bags.sort!
+bags.each do | i |
+    puts i
+end
+
+
+iCount = 3
+puts "last #{iCount} bags"
+i = 0
+iTotal = 0
+while (i < iCount)
+    puts "last bag: #{bags[bags.count - 1 - i]}"
+    iTotal += bags[bags.count - 1 - i]
+    i += 1
+end
+
+puts "Total #{iTotal}"
