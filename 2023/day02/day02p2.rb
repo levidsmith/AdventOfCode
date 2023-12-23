83f85d06ac014add0a40313e3ba9ff269e0440cd
Levi D. Smith <me@levidsmith.com>
2023-12-22 23:23:43 -0500

    Day 2, part 1 and 2

--- 2023/day02/day02p2.rb
+++ 2023/day02/day02p2.rb
@@ -1,15 +1,13 @@
 #2023 Levi D. Smith
-puts "Hello"
 def main() 
     iSum = 0
 
-    limits = {
-        "red" => 12,
-        "green" => 13,
-        "blue" => 14
-    }
-
     while (strLine = gets()) 
+		limits = {
+			"red" => 0,
+			"green" => 0,
+			"blue" => 0
+		}
         strLine.chomp!()
         strLine = strLine + ';'
         
@@ -23,6 +21,7 @@ def main()
         iStart = i + 1
         isValid = true
         while (i < strLine.length)
+
             
             if (strLine[i] == ',' or strLine[i] == ';')
                 strCubeCount = strLine[iStart...i].strip()
@@ -31,19 +30,23 @@ def main()
                 arrCubeCount = strCubeCount.split(' ')
                 iNum = arrCubeCount[0].to_i
                 strColor = arrCubeCount[1]
+				
+#				puts  "#{strColor}: #{iNum}"
 
                 if (iNum > limits[strColor])
-                    isValid = false
+                    limits[strColor] = iNum
                 end
             end
           
             i += 1
         end
+		
+#		puts "Game #{iGame}: red #{limits['red']}, green #{limits['green']}, blue #{limits['blue']}"
+		iPower = limits['red'] * limits['green'] * limits['blue']
+		puts "Game #{iGame}: power #{iPower}"
+		
+		iSum += iPower
 
-        if (isValid)
-            iSum += iGame
-
-        end
     end
 
     puts iSum
